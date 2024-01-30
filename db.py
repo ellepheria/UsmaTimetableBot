0f5f5842ccda0c469c8038446248b8a6076ee67e
Maxim Mineev <mms1401@ya.ru>
2024-01-30 18:20:35 +0500

    db api

---
+++ db.py
@@ -0,0 +1,48 @@
+import psycopg2
+import config
+
+
+def add_user(user):
+    sql = "INSERT INTO users (tg_id, tg_name, username) VALUES (%s, %s, %s)"
+    userdata = (user['id'], user['first_name'], user['username'])
+    conn = psycopg2.connect(dbname=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS, host=config.DB_HOST)
+    with conn.cursor() as cursor:
+        cursor.execute(sql, userdata)
+        conn.commit()
+    conn.close()
+
+
+def get_user(user_id):
+    sql = "SELECT * FROM users WHERE tg_id=%s"
+    conn = psycopg2.connect(dbname=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS, host=config.DB_HOST)
+    with conn.cursor() as cursor:
+        cursor.execute(sql, (user_id,))
+        result_user = cursor.fetchone()
+        conn.commit()
+    conn.close()
+
+    return {
+        'id': result_user[0],
+        'tg_id': result_user[1],
+        'name': result_user[2],
+        'username': result_user[3],
+    }
+
+
+def delete_user(user_id):
+    sql = "DELETE FROM users WHERE tg_id=%s"
+    conn = psycopg2.connect(dbname=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS, host=config.DB_HOST)
+    with conn.cursor() as cursor:
+        cursor.execute(sql, (user_id,))
+        conn.commit()
+    conn.close()
+
+
+def update_group(user_id, group):
+    sql = "UPDATE users SET \"group\"=%s WHERE tg_id=%s"
+    conn = psycopg2.connect(dbname=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS, host=config.DB_HOST)
+    data = (group, user_id)
+    with conn.cursor() as cursor:
+        cursor.execute(sql, data)
+        conn.commit()
+    conn.close()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import telebot
 
 import config
+import db
 
 
 bot = telebot.TeleBot(config.API_KEY)
@@ -8,6 +9,7 @@ bot = telebot.TeleBot(config.API_KEY)
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
+    db.add_user(message.from_user)
     if message.text == "Привет":
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
     elif message.text == "/help":
