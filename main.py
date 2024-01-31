ab0df68243b7f4580cca7ed6d39395ec54591de4
Maxim Mineev <mms1401@ya.ru>
2024-01-31 12:16:59 +0500

    /start logic

--- main.py
+++ main.py
@@ -2,20 +2,49 @@ import telebot
 
 import config
 import utils
+import constants
 from db import add_user, update_group, delete_user, get_user, get_group
 
 
 bot = telebot.TeleBot(config.API_KEY)
 
 
+def send_timetable_by_day():
+    pass
+
+
+def group_handler():
+    pass
+
+
 @bot.message_handler(commands=['start'])
 def get_text_messages(message):
-    if not get_user(message.from_user.id):
-        add_user(message.from_user)
+    if add_user(message.from_user):
+        bot.send_message(
+            message.from_user.id,
+            'Укажите Вашу группу (например, ЛД 101)'
+        )
+        return
+    group = get_group(message.from_user.id)
+    if not group:
+        bot.send_message(
+            message.from_user.id,
+            'Укажите Вашу группу (например, ЛД 101)'
+        )
+        return
+    bot.send_message(
+        message.from_user.id,
+        'Укажите необходимый день недели (например, ПН)'
+    )
+    return
+    # bot.send_message(message.from_user.id,
+    #                  utils.generate_message(message.from_user.first_name, utils.get_timetable('ЛД 101', 1))
+    #                  )
 
-    bot.send_message(message.from_user.id,
-                     utils.generate_message(message.from_user.first_name, utils.get_timetable('ЛД 101', 1))
-                     )
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+
 
 
 bot.polling(non_stop=True, interval=1)
