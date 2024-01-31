aabdbeaeb0c644bc77d39f2c424919cbfd15473a
Maxim Mineev <mms1401@ya.ru>
2024-01-31 12:30:18 +0500

    DRY

--- main.py
+++ main.py
@@ -9,44 +9,43 @@ from db import add_user, update_group, delete_user, get_user, get_group
 bot = telebot.TeleBot(config.API_KEY)
 
 
-def send_timetable_by_day():
-    pass
-
-
-def group_handler():
-    pass
-
-
-@bot.message_handler(commands=['start'])
-def get_text_messages(message):
-    if add_user(message.from_user):
-        bot.send_message(
-            message.from_user.id,
-            'Укажите Вашу группу (например, ЛД 101)'
-        )
-        return
-    group = get_group(message.from_user.id)
-    if not group:
-        bot.send_message(
-            message.from_user.id,
-            'Укажите Вашу группу (например, ЛД 101)'
-        )
-        return
+def ask_group(tg_id):
     bot.send_message(
-        message.from_user.id,
+        tg_id,
+        'Укажите Вашу группу (например, ЛД 101)'
+    )
+    return
+
+
+def ask_day(tg_id):
+    bot.send_message(
+        tg_id,
         'Укажите необходимый день недели (например, ПН)'
     )
     return
 
 
+@bot.message_handler(commands=['start'])
+def get_text_messages(message):
+    if add_user(message.from_user):
+        ask_group(message.from_user.id)
+        return
+    group = get_group(message.from_user.id)
+    if not group:
+        ask_group(message.from_user.id)
+        return
+    ask_day(message.from_user.id)
+    return
+
+
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
+    if message.text == constants.COMMANDS['GROUP_CHANGE']:
+        ask_group(message.from_user.id)
+        return
     if message.text in constants.GROUPS:
         update_group(message.from_user.id, message.text)
-        bot.send_message(
-            message.from_user.id,
-            'Укажите необходимый день недели (например, ПН)'
-        )
+        ask_day(message.from_user.id)
         return
     if message.text in constants.DAY_TO_DATE:
         group = get_group(message.from_user.id)
